61ed64478ff0d219e9a1ea28d5d9d2f220282d10
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-04-12 19:52:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ class Animal {
 }
 
 class Dog extends Animal {
-	purr(){
-		console.log("purr");
+	bark(){
+		console.log("woof");
 	}
 }
 
 class Cat extends Animal {
-	bark(){
-		console.log("woof");
+	purr(){
+		console.log("purr");
 	}
 }
 
